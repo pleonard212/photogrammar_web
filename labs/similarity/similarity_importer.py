8705357164e4897031e5e48f73d84d50477bb5dd
Peter Leonard <pleonard@gmail.com>
2014-06-23 10:40:11 -0400

    Update similarity_importer.py

--- labs/similarity/similarity_importer.py
+++ labs/similarity/similarity_importer.py
@@ -24,6 +24,7 @@ for line in docindexfile.readlines()[3:-1]:
 	line = line.replace('Key: ', '')
 	line = line.replace(': Value: /', ' ')	
 	line = line.replace('.txt', '')	
+	y = [value for value in line.split()]
 	docindex.append( y )
 
 docindexfile.close()
