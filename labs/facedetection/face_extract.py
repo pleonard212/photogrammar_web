8743e2c70433f6455a0489022aa7acf5d5654995
Peter Leonard <pleonard@gmail.com>
2014-11-09 21:44:38 -0500

    Fix loop logic

--- labs/facedetection/face_extract.py
+++ labs/facedetection/face_extract.py
@@ -2,11 +2,23 @@
 #Returns a subdirectory full of cropped faces, including multiple faces per image.
 import pandas
 from wand.image import Image
-from wand.display import display
-metadata = pandas.read_csv('metadata.csv')
+#from wand.display import display
+import os.path
+import sys
+counter = 0
+metadata = pandas.read_csv('photolists/'+sys.argv[1]+'-faces.csv')
 for idx, row in metadata.iterrows():
-	print "Now saving a face from " + row["File"] + " as " + str(idx) + ".jpg"
-	with Image(filename=row["File"]) as img:
+
+	with Image(filename='photolists/' + sys.argv[1] + '/' + row["File"]) as img:
+		
 		with img.clone() as i:
+			
 			i.crop(row["FrontalFace_X"],row["FrontalFace_Y"],width=row["FrontalFace_Width"],height=row["FrontalFace_Height"])
-			i.save(filename="facethumbs/"+(str(idx)+".jpg"))
+			print(("facethumbs/"+ os.path.splitext(row["File"])[0] + "_" + str(counter) +  ".jpg"))
+			if(os.path.isfile("facethumbs/"+ os.path.splitext(row["File"])[0] + "_"  + str(counter) +  ".jpg")):
+				counter += 1
+				print("Multiple faces in image " + row["File"])
+			else:
+				counter = 0
+			print "Now saving a face from " + row["File"] + " as " + "facethumbs/" + os.path.splitext(row["File"])[0] + "_" + str(counter)  +  ".jpg"
+			i.save(filename="facethumbs/"+os.path.splitext(row["File"])[0] + "_" + str(counter) + ".jpg")
